911d55d0a47f75504d72f4b344c66419e6813e30
travel-crazy <max.jeh9@gmail.com>
2026-03-16 15:42:43 +0000

    Add files via upload

---
+++ Chatbot (●'◡'●).py
@@ -0,0 +1,11 @@
+airline = input("What is your favourite airline?")
+if airline == "LOT":
+    print("Saaame")
+if airline == "Qantas":
+    print("Saaame")
+if airline == "Eurowings":
+    print("I mean they are most definitely the best BUDGET airline")
+if airline == "Ryanair":
+    print("No they're terrible, I flew Ryanair the other day and when we landed, I puked-plus I thought the landing gear collapesed!!＞︿＜")
+else:
+    print("Have a deeper think(´･ω･`)")
